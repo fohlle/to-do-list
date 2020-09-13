bf9f2287f153299b95df34acbc7e5931ddda8f21
fohlle <patte2000@hotmail.com>
2020-09-13 10:13:25 +0200

    simple to do list complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
 
 let btn = document.querySelector("#submit");
+let clear = document.querySelector("#clear");
+let uls = document.getElementById("list");
+uls.removeChild(uls.childNodes[0]);
 
 btn.addEventListener( "click" ,() => {
+    let ul = document.getElementById("list");
     let inputText = document.getElementById("input").value;
     let li = document.createElement("li");
-    let ul = document.getElementById("list");
-    li.textContent = inputText;
+    let length = ul.childNodes.length;
+    li.textContent = (length + 1) + ":" + inputText;
     ul.appendChild(li);
-    console.log(inputText);
+    document.getElementById("input").value = "";
 });
 
+clear.addEventListener("click", () => {
+    let list = document.querySelector("ul");
+    if(list.childNodes.length === 0)return;
+    while(list.childNodes.length > 0){
+        list.removeChild(list.childNodes[0]);
+    }
+
+})
